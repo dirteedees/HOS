88c5476a934f5c90575d6b06f89ee6576e61d1da
Jessie Barry Dees <jessie.barry.dees@gmail.com>
2016-04-06 17:45:03 -0500

    hos build as of 06.04.16

---
+++ deadpool.php
@@ -0,0 +1,75 @@
+
+<?php
+header('Location: /header.html');
+?>
+
+
+      </div>
+    <!--jumbotron-->
+        <div class="container-fluid">
+         <div class="center-block jumbotron">
+          <h1>DEADPOOL</h1>
+           <img src="images/BIG%20HIT.png" alt="hit">      
+            <p  class="text-center review ">“Deadpool”- by now you’ve seen him all over FYE and Hot Topic. He’s that “LOL SO RANDUMB” comic book character fifteen year olds love. I believe he is the first and so far only comic book character to basically become well known solely because of the Internet. He’s a violent, smartass, fourth wall breaking high energy character, but how well does an internet sensation translate to cinema?
+                <br><br>
+I went in having seen the trailers, which mostly made me cringe. Thank God (and to my surprise), the movie actually has a fair bit of heart in it. Sandwiched between jokes that range from cringe to clever are moments of genuine heart and emotion. Yes, the movie actually has a semi-decent (although generic) story.
+                <br><br>
+“Deadpool” (Ryan Reynolds in an example of perfect casting) is a science experiment gone horribly wrong. He is a horribly disfigured man seeking revenge on his creators. Original, right? Where “Deadpool” succeeds so well is in the way it tells its generic story. Instead of wallowing around and bitching, “Deadpool” instead presents its story in a violent, high energy manner, which goes along perfectly with its darker, more heartfelt origin story flashbacks. Mentioning origin story: Thank God the writers really kinda breeze over it. The whole movie builds at such a rapid pace, it isn’t until the final scene that the whole “I’m so edgy” smartass humor starts to wear down.
+                <br><br>
+All the actors do well in their roles, but they aren’t given much to work with. They all exist to either direct or banter with Deadpool, that’s it. The direction is more than competent, and the action scenes are gleefully over-the-top and bloody. They also have almost zero shakey cam, which is fucking great. The writing for the most part is good, but with so many jokes, some are bound to miss, and the fourth wall breaking seemed to confuse the audience I was with.
+                <br><br>
+My only real complaint is that the movie thinks it is far smarter than it actually is. Every time something “generic” happens, Deadpool will turn to the viewer and wink and make some smartass crack. Sorry writers, being aware of your writing doesn’t make it good. This is a small complaint, though, as I’m still blown away with how well this movie turned and how big of a disaster the whole crew avoided.
+                <br><br>
+In every single way, Deadpool’s second appearance on screen (ses I’ve sadly got to count his origin’s “appearance”) fares MILES above his first. It also fares a lot more favorably than many recent comic adaptations. Fans and newcomers alike will love the merc with a mouth’s zany, self-aware brand of storytelling. While the movie may appear “edgy” and “different,” it actually plays it safe with a studio approved, “Hot Topic t-shirt ready” version of the fan favorite character.
+<br><br/>
+Highlights: Very faithful representation of the character, earns itso R rating, Reynolds is perfect
+        <br><br>        
+Lowlights: Only appears to be different, falls into a lot of the same “superhero” clichés it tries so hard to subvert, a lot of jokes miss
+  
+</p>
+         
+            
+          <img src="images/Deadpool.JPG"  alt="DEADPOOL">
+        
+            </div>
+        </div>
+        
+        <div class="panel-group" id="accordion">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+              <img src="images/Profiles/dees.png" alt="Dees avi">
+              2nd Thought
+              <img src="images/BIG%20HIT.png" width="3%">//<img src="images/BIG%20SHIT.png" width="3%">
+            </a>
+          </h4>
+      </div>
+      <div id="collapse1" class="panel-collapse collapse in">
+        <div class="panel-body">
+            <p class="collapsetext" >OK At first it didn't know what to say or how to say it about DEADPOOL. But after about a week I think I'm ready. You may be wondering why the HIT and SHIT badges, there's good reason. Ok first off I got erked at the previews before the moive they were shit moives using Queen for the music. (One of the best bands of all time) That's blasphemy to me, needless to say I wasn't happy. But the moive starts and it rolling "funny" intro credits. 3/10. So the moives goes on I get a hanful of chuckles, meanwhile tumblr is busting a gut behind me. This lead me to have such a undesirable viewing. I would've liked it much more in a home theater but good more, didn't live up to the hype for me though. (STORY TIME) So the moive ends and the theater is in a mall. We go out a way that was not the way we came in, So i'm all twisted around. We end up walking all the way around the mall to get back to the car. Turn the corner see the car.</p>  <div id="deadpool2ndthought"> >>It's almost over >>See odd car sitting near ride  with light and shit on >>Oh fuck were gonna get mugged >>Don't >>Go home </div> 
+           
+          </div>
+      </div>
+    </div>
+    </div>
+     
+        
+    </div>
+      
+   <!-- right "nav"-->
+      <div class="col-sm-2 sidenav">
+         <a class="btn btn-default btn-lg" href="oscar.html" role="button">Oscar Nominations</a>
+      </div>
+  </div>
+    
+   
+</div>
+           
+ <!-- DO NOT TOUCH FOOTER !!! -->
+<footer class="container-fluid text-center">
+  <i>"One jaded hipster's opinion"</i>
+</footer>
+
+</body>
+</html>
---
+++ header.asp
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!--#include file="hosutils.asp"-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title><%Response.write("Hit or shit") %></title>
+    <link rel="shortcut icon" href="favicon.ico" />
+    <!-- Bootstrap -->
+    <link href="js/bootstrap.min.js" rel="script">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!--my css-->
+    <link href="css/hos.css" rel="stylesheet" type="text/css">
+    <!--fonts-->
+    <link href='https://fonts.googleapis.com/css?family=Press+Start+2P' rel='stylesheet' type='text/css'>
+    <link href="fonts/gameover.ttf" rel="stylesheet" type="text/css">
+</head>
+
+<html lang="en">
+<body>
+
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Logo</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.html">Home</a></li>
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+    <div class="container-fluid ">
+        <div class="row content">
+
+            <!-- left nav-->
+            <div class="col-sm-2 sidenav">
+                <!--left nav buttons-->
+
+                <div class="center-block btn-group-vertical" role="group" aria-label="...">
+                    <a class="btn btn-default btn-lg" href="#" role="button">Newest</a>
+                    <a class="btn btn-default btn-lg" href="#" role="button">Reviews</a>
+                    <a class="btn btn-default btn-lg" href="Bios.html" role="button">Bios</a>
+                    <a class="btn btn-default btn-lg" disabled="disabled" href="#" role="button">Forum(coming soon)</a>
+                    <a class="btn btn-default btn-lg" disabled="disabled" href="#" role="button">Hall of Fame-Shame</a>
+                    <a class="btn btn-default btn-lg" href="pastpicks.html" role="button">Past Picks</a>
+                </div>
+
+                <!--pook's pick-->
+                <div class="pookspick">
+
+                    <p class="text-center">Pook's Pick</p>
+                    <br>
+                    <a href="http://www.imdb.com/title/tt0063850/" target="_blank">
+                        <img class=" pppic center-block" src="images/if.jpg" alt="pppic">
+                    </a>
+                </div>
+
+            </div>
+
+            <!-- center panel-->
+            <div class="col-sm-8 text-left">
+                <div class="center-block titlebar">
+                    <h1 class="text-center title">Hit or Shit </h1>
+
+                    <div class="hsbadge">
+                        <img class="center-block" src="images/HIT%20or%20SHIT%202.png" alt="hsbadge">
+                    </div>
+                </div>
+</html>
