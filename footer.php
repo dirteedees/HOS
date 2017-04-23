eb44914f72b2746a15f639d3d352be530e57e99f
Kurtis Graben <kurtis.graben@me.com>
2017-04-23 00:20:51 -0500

    New files
    This adds new files that seperate different parts of the index (and
    others) page that are repetitive and need to be dynamic.

---
+++ footer.php
@@ -0,0 +1,6 @@
+<!-- DONT TOUCH -->
+    </div>
+</div>
+<footer class="container-fluid text-center">
+    <em>"One jaded hipster's opinion"</em>
+</footer>
---
+++ nav.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-inverse tnavbar">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+
+                </button>
+
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class="active" id="NavHome"><a href="index.php">Home</a></li>
+                    <li class="active" id="NavTech"><a href="tech.php">Tech</a></li>
+                    <li class="active" id="NavGames"><a href="Games_header.php">Games</a></li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid text-center">
+        <div class="row content">
+          <!-- left nav-->
+            <div class="col-sm-2 sidenav">
+               <!--left nav buttons-->
+              <div class="mobilebuttons">
+                <div class="center-block btn-group-vertical" role="group" aria-label="...">
+                <a class="btn btn-default btn-lg btn" id="reviews" onmouseover="rngcolor();" href="reviews.php" role="button">Reviews</a>
+                <a class="btn btn-default btn-lg btn" id="bios" onmouseover="rngcolor();" href="bios.php" role="button">Bios</a>
+                <a class="btn btn-default btn-lg btn" id="hofs" onmouseover="rngcolor();" href="Hall_of_shame-fame.html" role="button">Hall of Fame-Shame</a>
+                <a class="btn btn-default btn-lg btn" id="pastp" onmouseover="rngcolor();" href="pastpicks.php" role="button">Past Picks</a>
+                <!--  <a class="btn btn-lg tbtn" disabled="disabled" id="forum" onmouseover="rngcolor();" href="#" role="button">Forum (coming soon)</a> -->
+                </div>
