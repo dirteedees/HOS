c729b374429f180e93ff238229c849e557337ada
Jessie Barry Dees <jessie.barry.dees@gmail.com>
2017-04-29 18:30:21 -0500

    twitter and tech now in the php game
    edited css to alow for tech header as well as added twitter feed  that goes in right coloum of HoS

---
+++ twitfeed.php
@@ -0,0 +1,9 @@
+<!-- right "nav"-->
+<div class="col-sm-2 ">
+    <!-- twitter feed -->
+    <div id="twit_feed">
+        <h3>Twits</h3>
+        <a class="twitter-timeline" data-width="250" data-height="1000" data-dnt="true" data-chrome="transparent noscrollbar nofooter noheader" data-theme="dark" data-link-color="#3370ff" href="https://twitter.com/hitorshitmovies">Tweets by hitorshitmovies</a>
+        <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
+</div>
