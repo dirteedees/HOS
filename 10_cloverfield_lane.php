34fd4fa53e6fc7c34aab874a19b20c859e310659
Jessie Barry Dees <jessie.barry.dees@gmail.com>
2017-06-18 23:05:02 -0500

    (Project Shelved) last changes added

---
+++ 10_cloverfield_lane.php
@@ -0,0 +1,62 @@
+<?php
+require('header.php');
+require('nav.php');
+require('pooks_pick.php');
+require('center_panel.php');
+?>
+    <!--jumbotron-->
+        <div class="container-fluid">
+         <div class="center-block jumbotron">
+              <h1>10 Cloverfield Lane</h1>
+               <img class="center-block img-responsive" src="images/10cloverfieldlane.JPG" alt="poster" width="10%">
+             <img class="rating" src="images/BIG%20HIT.png" alt="grinding tonight">
+             <p style=" color: black;">No snarky or “clever” opening here, I’m just gonna get it out of the way. This is the question everyone wants to know the answer to: “Is it a sequel to Cloverfield?”<br><br>
+                                                                        <u><b>SPOILERS</b></u>
+                                                                         <br><br>
+                                            <u><b>LIKE NO QUIT SCROLLING I’M SPOILING THIS SHIT</b></u>
+                                                                 <br><br>
+                                                                <u><b>HERE WE GO</b></u>
+                                                                         <br><br>
+                 It is not. Not even a little bit.
+ <br><br>
+Surely by now, we all know that “10 Cloverfield Lane” started as a completely different and standalone
+                 movie. It had nothing to do with “Cloverfield,” and it still doesn’t. Now let me take a break while some JJ
+                 fanboy points out that an AR game has connections. Yeah, that doesn’t count.<br><br>
+
+So now that we’ve covered that the two aren’t connected except for marketing reasons, how is the
+                 movie? It’s pretty good. <br><br>
+
+The movie is very Hitchcock in the way it builds tension, atmosphere and paints the characters. Three
+                 people are locked in an underground bunker. Howard (John Goodman) tells the other two that the world
+                 has ended, and it is no longer safe to go outside. Because of basic human nature and a few questionable
+                 holes in Howard’s story, Michelle (Mary Elizabeth Winstead) begins to plot an escape. That’s all the detail
+                 I’ll go into plot-wise.<br><br>
+
+First time director Dan Trachtenberg guides the movie with a masterful hand, hitting all the right notes
+                 from laughter to seriously intense moments. Dan directs this movie with an almost unheard of amount
+                 of confidence, especially for a first timer. The movie had some moments that made me lean forward and
+                 kind of tense up, which is rare for me. The acting put on display here is great by all; to me, all the
+                 performances felt pretty equal. The cinematography is perfectly okay. Not all that much you can do in a
+                 small bunker, is there?<br><br>
+
+“10 Cloverfield Lane” would be that kind of “Hey, this is really good check it out” under-the-radar movie
+                 had it stuck with its original title “The Cellar.” With this new title slapped on to it, the movies attracts
+                 expectations it will not nor could ever meet. I can see it falling prey to the “Curse of the Witch,” my
+                 not-subtle-at-all reference to the backlash garnered by “Halloween 3: Season of the Witch” upon its
+                 initial release. While people are wrong to judge this movie’s merits based on its marketing, so are the
+                 people who are claiming this movie is great and circle jerking it just to seem “enlightened” or whatever.
+                 “Woah bruh you didn’t like ‘10 Cloverfield Lane’ just because the marketing mislead you? Bruh you need
+                 to get smart and all-knowing like me LOL.” The movie is a pretty good, under-the-radar sort of thing that
+                 only attracted hype based on its title. That’s all and nothing more.<br><br>
+
+Go in with no expectations, and I bet you’ll be surprised. Go in with expectations, and I’ll see you
+                 <br>chugging Mountain Dew and hammering away on your keyboard throwing a tantrum.<br><br>
+
+<b>Highlights</b>: Great atmosphere and tension, acting is tops, direction is confident <br>
+<b>Lowlights</b>: The last act, its title, not at all original
+</p>
+        </div>
+        </div>
+
+</body>
+</html>
